e7d66eb412768ed29443c51a7b65dc4280c3e00f
Nikhil sai <ks8274@srmist.edu.in>
2026-06-22 00:23:45 +0530

    Fix production API baseURL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://splitwise-api.nikhilsai.uk/api',
+  baseURL: 'https://splitwise-api.nikhilsai.uk/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
